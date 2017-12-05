53944e9fc9e24a4a5a8eb29a824471ae03f6a661
Tikky Shiwala <tshiwala@constantcontact.com>
2017-12-05 12:21:08 -0500

    test seed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
   has_many :votes
+
+  COUNTRY_LIST = %w(U.S.A Canada D.R.C Belgium France U.K)
+
+  validates_presence_of :country, :state, :city
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Vote.destroy_all
+Candidate.destroy_all
+Location.destroy_all
+
+c1 = Candidate.create(first_name: 'Tikky', last_name: 'Shiwala')
+c2 = Candidate.create(first_name: 'Lionel', last_name: 'Shiwala')
+c3 = Candidate.create(first_name: 'Ben', last_name: 'Shiwala')
+
+l1 = Location.create(country: 'U.S.A', state: 'Massachussets', city: 'Boston', name: 'awa')
+l2 = Location.create(country: 'U.S.A', state: 'Massachussets', city: 'Lynn', name: 'kuna')
+
+Vote.create(location: l1, candidate: c1, online_count: 12, offline_count: 43)
+Vote.create(location: l1, candidate: c2, online_count: 5, offline_count: 10)
+Vote.create(location: l2, candidate: c1, online_count: 15, offline_count: 50)
+Vote.create(location: l2, candidate: c2, online_count: 10, offline_count: 53)
